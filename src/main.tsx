605ac5e1643a040a2125d3e348597586685c5e53
Lamylbill <78673748+Lamylbill@users.noreply.github.com>
2025-04-02 22:51:03 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
